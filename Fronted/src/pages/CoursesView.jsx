3bd9cb846fd2639be9d4e8e08aae34eee977ef43
Zarar-Anwar <zararanwar1234321@gmail.com>
2023-06-07 13:06:03 +0500

    Course View Page : 80%

--- Fronted/src/pages/CoursesView.jsx
+++ Fronted/src/pages/CoursesView.jsx
@@ -12,44 +12,23 @@ function CoursesView() {
                 <div class="col-md-12 col-lg-8 col-xl-9">
               <div class="has-blocks" aria-label="Content">
                   <aside id="block-region-above-content" class="block-region" data-blockregion="above-content" data-droptarget="1"><div id="inst107" class=" block_cocoon_course_intro block " role="complementary" data-block="cocoon_course_intro" aria-label="[Cocoon] Course Intro">
-
-
-
-
-
   <div class="">
-    
-
         <div class="cs_row_one ">
   <div class="cs_ins_container">
   <div class="ccn-identify-course-intro">
     <div class="cs_instructor">
       <ul class="cs_instrct_list float-left mb0">
-        <li class="list-inline-item"><img class="thumb" src="http://demo.createdbycocoon.com/moodle/edumy/2/pluginfile.php/162/block_cocoon_course_intro/content/4.png" alt="4.png"/></li>
-        <li class="list-inline-item"><a class="" href="#">Ali Tufan</a></li>
-        <li class="list-inline-item"><a class="" href="#">Last updated 21 December 2019</a></li>
-      </ul>
-      <ul class="cs_watch_list float-right mb0">
-        <li class="list-inline-item"><a class="" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http://demo.createdbycocoon.com/moodle/edumy/2/course/view.php?id=11"><span class="flaticon-share"> Share</span></a></li>
+        <li class="list-inline-item"><strong>{data.createdAt.split('T')[0]} </strong></li>
       </ul>
     </div>
-    <h3 class="cs_title ">Fashion Photography From Professional</h3>
-    <ul class="cs_review_seller">
-      <li class="list-inline-item"><a href="#"><span>Best Seller</span></a></li>
-      <li class="list-inline-item"><i class="fa fa-star"></i></li>
-      <li class="list-inline-item"><i class="fa fa-star"></i></li>
-      <li class="list-inline-item"><i class="fa fa-star"></i></li>
-      <li class="list-inline-item"><i class="fa fa-star"></i></li>
-      <li class="list-inline-item"><i class="fa fa-star"></i></li>
-    </ul>
+    <h3 class="cs_title ">{data.name}</h3>
     <ul class="cs_review_enroll">
-      <li class="list-inline-item"><a class="" href="#"><span class="flaticon-profile"></span> 2 students enrolled</a></li>
-      <li class="list-inline-item"><a class="" href="#"><span class="flaticon-comment"></span> 5 topics</a></li>
+      <li class="list-inline-item" style={{color:'grey',fontSize:'30px'}}>Purchased to Watch Video</li>
     </ul>
     </div>
     <div class="courses_big_thumb">
       <div class="thumb">
-        <iframe class="iframe_video" src="//www.youtube.com/embed/57LQI8DKwec" frameborder="0" allowfullscreen=""></iframe>
+        <img style={{maxHeight:"300px",width:"600px"}} src={data.thumbnail_image} alt="Course Image" />
       </div>
     </div>
   </div>
@@ -73,55 +52,27 @@ function CoursesView() {
           <div class="cs_overview">
             <h4 class="title">Course Overview</h4>
             <h4 class="subtitle">Course Description</h4>
-<p class="mb30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-    survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-<p class="mb20">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-<h4 class="subtitle">What you'll learn</h4>
+<p class="mb30">{data.description} </p>
+<h4 class="subtitle">Detail Description</h4>
 <ul class="cs_course_syslebus">
-    <li><i class="fa fa-check"></i>
-        <p>Become a UX designer.</p>
+    <li>
+        <p>{data.detail_description}</p>
     </li>
-    <li><i class="fa fa-check"></i>
-        <p>You will be able to add UX designer to your CV</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>Become a UI designer.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>Build &amp; test a full website design.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>Build &amp; test a full mobile app.</p>
-    </li>
+   
 </ul>
-<ul class="cs_course_syslebus2">
-    <li><i class="fa fa-check"></i>
-        <p>Learn to design websites &amp; mobile phone apps.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>You'll learn how to choose colors.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>Prototype your designs with interactions.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>Export production ready assets.</p>
-    </li>
-    <li><i class="fa fa-check"></i>
-        <p>All the techniques used by UX professionals</p>
-    </li>
-</ul>
+
 <h4 class="subtitle">Requirements</h4>
 <ul class="list_requiremetn">
     <li><i class="fa fa-circle"></i>
-        <p>You will need a copy of Adobe XD 2019 or above. A free trial can be downloaded from Adobe.</p>
+        <p>If you dont know the code its Fine</p>
     </li>
     <li><i class="fa fa-circle"></i>
-        <p>No previous design experience is needed.</p>
+        <p>You will Learn The Course from Scratch. </p>
     </li>
     <li><i class="fa fa-circle"></i>
-        <p>No previous Adobe XD skills are needed.</p>
+        <p>Here we provide quality Course with Easy Methods and Practise</p>
     </li>
+    
 </ul>
           </div>
         </div>
@@ -182,44 +133,7 @@ function CoursesView() {
     
 
 
-        <div class="cs_row_four">
-  <div class="about_ins_container">
-    <h4 class="aii_title">About the instructor</h4>
-    <div class="about_ins_info">
-      <div class="thumb"><img src="http://demo.createdbycocoon.com/moodle/edumy/2/pluginfile.php/164/block_cocoon_course_instructor/content/6.png" alt="6.png"/></div>
-    </div>
-    <div class="details">
-      <ul class="review_list">
-        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-        <li class="list-inline-item">4.5 Instructor Rating</li>
-      </ul>
-      <ul class="about_info_list">
-        <li class="list-inline-item"><span class="flaticon-comment"></span> 12,197 Reviews </li>
-        <li class="list-inline-item"><span class="flaticon-profile"></span> 141,745 Students </li>
-        <li class="list-inline-item"><span class="flaticon-play-button-1"></span> 5 Courses </li>
-      </ul>
-      <h4>Ali Tufan</h4>
-      <p class="subtitle">UX/UI Designer</p>
-      <p class="mb25">UI/UX Designer, with 7+ Years Experience. Guarantee of High Quality Work. </p>
-<p class="mb25">Skills: Web Design, UI Design, UX/UI Design, Mobile Design, User Interface Design, Sketch, Photoshop, GUI, Html, Css, Grid Systems, Typography, Minimal, Template, English, Bootstrap, Responsive Web Design, Pixel Perfect, Graphic Design, Corporate, Creative,
-    Flat, Luxury and much more.</p>
-<ul class="about_ins_list mb0">
-    <li>
-        <p>Available for:</p>
-    </li>
-    <li><a href="#">1. Full Time Office Work</a></li>
-    <li><a href="#">2. Remote Work</a></li>
-    <li><a href="#">3. Freelance</a></li>
-    <li><a href="#">4. Contract</a></li>
-    <li><a href="#">5. Worldwide</a></li>
-</ul>
-    </div>
-  </div>
-</div>
+      
     
     
   </div>
@@ -234,15 +148,7 @@ function CoursesView() {
 
 
 
-  <div class="">
-    <div style={{display:'none'}} id="cmt-tmpl"><div class="comment-message media csv1"><div class="comment-message-meta mr-3"><span class="picture">___picture___</span> - </div><div class="text media-body"><h4 class="sub_title mt-0 mb0">___name___</h4><span class="sspd_postdate fz14">___time___</span>___content___</div></div><div class="custom_hr"></div></div><div class="mdl-left"><a class="showcommentsnonjs" href="http://demo.createdbycocoon.com/moodle/edumy/2/course/view.php?id=11&amp;nonjscomment=1&amp;comment_itemid=0&amp;comment_context=156&amp;comment_component=block_comments&amp;comment_area=page_comments">Show comments</a><div id="comment-ctrl-64801c8a3ed49" class="comment-ctrl" style={{display:'block'}}><ul id="comment-list-64801c8a3ed49" class="comment-list comments-loaded"><li class="first"></li><li id="comment-19-64801c8a3ed49"><div class="comment-message media csv1"><div class="comment-message-meta mr-3"><span class="picture"><a href="http://demo.createdbycocoon.com/moodle/edumy/2/user/view.php?id=5&amp;course=11"><img src="http://demo.createdbycocoon.com/moodle/edumy/2/pluginfile.php/222/user/icon/edumy/f2?rev=3801" class="align-self-start mr-3 ccn_userpicture" width="18" height="18" alt="Picture of Krisztina Szer" title="Picture of Krisztina Szer"/></a></span> - </div><div class="text media-body"><h4 class="sub_title mt-0 mb0">Krisztina Szer</h4><span class="sspd_postdate fz14">Fri, 3 Jan 2020, 12:54 AM</span><div class="no-overflow"><div class="text_to_html fz15 mt20">This is the second Photoshop course I have completed with Cristian. Worth every penny and recommend it highly. To get the most out of this course, its best to to take the Beginner to Advanced course first.<br/>
-<br/>
-The sound and video quality is of a good standard. Thank you Cristian.</div></div></div></div><div class="custom_hr"></div></li><li id="comment-27-64801c8a3ed49"><div class="comment-message media csv1"><div class="comment-message-meta mr-3"><span class="picture"><a href="http://demo.createdbycocoon.com/moodle/edumy/2/user/view.php?id=5&amp;course=11"><img src="http://demo.createdbycocoon.com/moodle/edumy/2/pluginfile.php/222/user/icon/edumy/f2?rev=3801" class="align-self-start mr-3 ccn_userpicture" width="18" height="18" alt="Picture of Krisztina Szer" title="Picture of Krisztina Szer"/></a></span> - </div><div class="text media-body"><h4 class="sub_title mt-0 mb0">Krisztina Szer</h4><span class="sspd_postdate fz14">Fri, 3 Jan 2020, 12:55 AM</span><div class="no-overflow"><div class="text_to_html fz15 mt20">This is the second Photoshop course I have completed with Cristian. Worth every penny and recommend it highly. To get the most out of this course, its best to to take the</div></div></div></div><div class="custom_hr"></div></li></ul><div id="comment-pagination-64801c8a3ed49" class="comment-pagination"></div></div></div>
-    
-    
-  </div>
-
-    <span id="sb-9"></span>
+    {/* <span id="sb-9"></span> */}
 
 </div><div id="inst147" class=" block_cocoon_more_courses block " role="complementary" data-block="cocoon_more_courses">
 
@@ -400,10 +306,15 @@ The sound and video quality is of a good standard. Thank you Cristian.</div></di
   <div class="">
       <div class="instructor_pricing_widget">
                                       <div class="price">
-                                        <span>Price</span> $49.99 <small>USD</small>
+                                        <span>Price</span> ${data.price} <small>USD</small>
                                       </div>
                                       <a href="http://demo.createdbycocoon.com/moodle/edumy/2/enrol/index.php?id=11" class="cart_btnss">Buy &amp; Enrol Now</a>
-                                      <div class="ccn-buy-access">Paid course entry</div><ul class="price_quere_list text-left"><li><div class="ccn-course-details-item"><span class="flaticon-play-button-1"></span> 11 hours on-demand video</div></li><li><div class="ccn-course-details-item"><span class="flaticon-download"></span> 69 downloadable resources</div></li><li><div class="ccn-course-details-item"><span class="flaticon-key-1"></span> Full lifetime access</div></li><li><div class="ccn-course-details-item"><span class="flaticon-responsive"></span> Access on mobile and TV</div></li><li><div class="ccn-course-details-item"><span class="flaticon-flash"></span> Assignments</div></li><li><div class="ccn-course-details-item"><span class="flaticon-award"></span> Certificate of Completion</div></li></ul>
+                                      <div class="ccn-buy-access">Paid course entry</div><ul class="price_quere_list text-left">
+                                        <li><div class="ccn-course-details-item"><span class="flaticon-play-button-1"></span>Video of Course</div></li>
+                                        <li><div class="ccn-course-details-item"><span class="flaticon-download"></span>Many Download Resourses</div></li>
+                                        <li><div class="ccn-course-details-item"><span class="flaticon-key-1"></span> Full lifetime access</div></li>
+                                        <li><div class="ccn-course-details-item"><span class="flaticon-responsive"></span> Access on mobile and TV</div></li>
+                                        <li><div class="ccn-course-details-item"><span class="flaticon-award"></span>Excersie</div></li></ul>
           </div>
     
     
@@ -426,9 +337,7 @@ The sound and video quality is of a good standard. Thank you Cristian.</div></di
             <li class="d-flex justify-content-between align-items-center">
                 Lectures <span class="float-right">6</span>
             </li>
-            <li class="d-flex justify-content-between align-items-center">
-                Quizzes <span class="float-right">1</span>
-            </li>
+            
             <li class="d-flex justify-content-between align-items-center">
                 Duration <span class="float-right">3 hours</span>
             </li>
@@ -452,36 +361,6 @@ The sound and video quality is of a good standard. Thank you Cristian.</div></di
 </div><div id="inst113" class=" block_tags block " role="complementary" data-block="tags">
 
     <a href="#sb-3" class="sr-only sr-only-focusable">Skip Tags</a>
-
-
-
-
-
-  <div class="">
-    <div class="blog_tag_widget">
-  <h4 class="title">Tags</h4>
-  <ul class="tag_list">
-        <li class="list-inline-item">
-            <a href="http://demo.createdbycocoon.com/moodle/edumy/2/tag/index.php?tc=1&amp;tag=Beginner&amp;from=156" class=" s10" title="Entries: 1">
-                        Beginner</a>
-        </li>
-        <li class="list-inline-item">
-            <a href="http://demo.createdbycocoon.com/moodle/edumy/2/tag/index.php?tc=1&amp;tag=Marketing&amp;from=156" class=" s10" title="Entries: 1">
-                        Marketing</a>
-        </li>
-        <li class="list-inline-item">
-            <a href="http://demo.createdbycocoon.com/moodle/edumy/2/tag/index.php?tc=1&amp;tag=Photoshop&amp;from=156" class=" s20" title="Entries: 2">
-                        Photoshop</a>
-        </li>
-        <li class="list-inline-item">
-            <a href="http://demo.createdbycocoon.com/moodle/edumy/2/tag/index.php?tc=1&amp;tag=Sketch&amp;from=156" class=" s20" title="Entries: 2">
-                        Sketch</a>
-        </li>
-  </ul>
-</div>
-    
-    
-  </div>
 
     <span id="sb-3"></span>
 
